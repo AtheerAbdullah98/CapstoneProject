648acc5ad7754c47f55459eefae130e0ea26fa09
nada <nada_abdullah_8@hotmail.com>
2023-09-11 10:35:11 +0300

    حل مشكله البروفايل الغير ظاهر

--- Capstone/accounts/views.py
+++ Capstone/accounts/views.py
@@ -12,7 +12,10 @@ def register_user_view(request : HttpRequest):
     if request.method == "POST":
         new_user = User.objects.create_user(first_name=request.POST["first_name"], last_name=request.POST["last_name"], username=request.POST["username"], email=request.POST["email"], password=request.POST["password"], )
         new_user.save()
+        profile=Profile.objects.create(user=new_user)
+        profile.save()
         return redirect("accounts:login_user_view")
+    
 
     return render(request, "accounts/register.html")
 
