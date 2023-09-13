40c8fe89b83f4f0a5fe6c76218b296f308730f9e
nada <nada_abdullah_8@hotmail.com>
2023-09-13 10:56:20 +0300

    view account

--- Capstone/accounts/views.py
+++ Capstone/accounts/views.py
@@ -33,7 +33,7 @@ def login_user_view(request: HttpRequest):
             login(request, user)
             return redirect("main:home_view")
         else:
-            msg = "Username or password is no correct. No user found."
+            msg = "اسم المستخدم او رمز المرور غير صحيحه"
 
 
     return render(request, "accounts/registration.html", {"msg": msg})
